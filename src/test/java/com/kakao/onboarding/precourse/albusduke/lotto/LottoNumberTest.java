09bc6d94743467e11422ab04b05ccfb39c794981
albus.kim <mman011539@gmail.com>
2026-02-10 17:12:40 +0900

    test: LottoNumber 테스트 보완 (equals, hashCode, compareTo)

--- src/test/java/com/kakao/onboarding/precourse/albusduke/lotto/LottoNumberTest.java
+++ src/test/java/com/kakao/onboarding/precourse/albusduke/lotto/LottoNumberTest.java
@@ -27,5 +27,52 @@ public class LottoNumberTest {
         ).doesNotThrowAnyException();
     }
 
+    @Test
+    void 최소값_1로_로또_번호를_생성할_수_있다() {
+        assertThatCode(() ->
+                new LottoNumber(1)
+        ).doesNotThrowAnyException();
+    }
 
+    @Test
+    void 최대값_45로_로또_번호를_생성할_수_있다() {
+        assertThatCode(() ->
+                new LottoNumber(45)
+        ).doesNotThrowAnyException();
+    }
+
+    @Test
+    void 같은_숫자로_생성된_로또_번호는_같다() {
+        LottoNumber number1 = new LottoNumber(10);
+        LottoNumber number2 = new LottoNumber(10);
+
+        assertThat(number1).isEqualTo(number2);
+        assertThat(number1.hashCode()).isEqualTo(number2.hashCode());
+    }
+
+    @Test
+    void 다른_숫자로_생성된_로또_번호는_다르다() {
+        LottoNumber number1 = new LottoNumber(10);
+        LottoNumber number2 = new LottoNumber(20);
+
+        assertThat(number1).isNotEqualTo(number2);
+    }
+
+    @Test
+    void 로또_번호를_비교할_수_있다() {
+        LottoNumber number1 = new LottoNumber(10);
+        LottoNumber number2 = new LottoNumber(20);
+        LottoNumber number3 = new LottoNumber(10);
+
+        assertThat(number1.compareTo(number2)).isLessThan(0);
+        assertThat(number2.compareTo(number1)).isGreaterThan(0);
+        assertThat(number1.compareTo(number3)).isEqualTo(0);
+    }
+
+    @Test
+    void 로또_번호를_가져올_수_있다() {
+        LottoNumber number = new LottoNumber(10);
+
+        assertThat(number.getNumber()).isEqualTo(10);
+    }
 }
