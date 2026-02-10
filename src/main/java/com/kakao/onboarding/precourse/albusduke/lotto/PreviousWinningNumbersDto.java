226efe159a3de97a09998d6fe391af2ecb53c0dc
albus.kim <mman011539@gmail.com>
2026-02-10 17:03:51 +0900

    refactor: DTO 클래스 제거 및 PurchaseAmount로 리팩토링

--- src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/PreviousWinningNumbersDto.java
+++
@@ -1,6 +0,0 @@
-package com.kakao.onboarding.precourse.albusduke.lotto;
-
-import java.util.List;
-
-public record PreviousWinningNumbersDto(List<Integer> winingNumbers, int bonusNumber) {
-}
--- src/main/java/com/kakao/onboarding/precourse/albusduke/lotto/PurchasedLottoCountDto.java
+++
@@ -1,4 +0,0 @@
-package com.kakao.onboarding.precourse.albusduke.lotto;
-
-public record PurchasedLottoCountDto(int count) {
-}
